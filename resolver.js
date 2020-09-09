a9be1c467c93e5a8f889f6e11e81ab4b8a939266
吴学泳 <786657219@qq.com>
2020-09-09 16:20:52 +0800

    完成多接口请求需求数据功能

--- resolver.js
+++ resolver.js
@@ -1,16 +1,23 @@
 const createHTTP = require('./http')
 
 module.exports = (req, res, gqlParams) => {
+  const commonHeaders = {
+    'Content-Type': 'application/json;charset=UTF-8',
+    'Access-Token': req.headers['access-token']
+  }
   const http = createHTTP({
+    headers: commonHeaders
+  })
+  const http2 = createHTTP({
     headers: {
-      'Content-Type': 'application/json;charset=UTF-8',
-      'Access-Token': req.headers['access-token']
+      ...commonHeaders,
+      'Api-Version': 2
     }
   })
 
   // 获取报警日志
   const fetchAlertLogs = (projectId, params) => 
-    http.post(`realty-master-data/projects/${projectId}/alert/rules`, params)
+    http.post(`realty-master-data/projects/${projectId}/alarm/state-reccords`, params)
 
   // 获取产品数据端点
   const fetchDataPoints = async productId => {
@@ -83,7 +90,7 @@ module.exports = (req, res, gqlParams) => {
         data: {
           data: { list },
         },
-      } = await getAlertRuleList(projectId, params)
+      } = await http2.post(`realty-master-data/projects/${projectId}/alert/rules`, params)
       
       return list
     } catch (e) {
@@ -93,65 +100,99 @@ module.exports = (req, res, gqlParams) => {
 
   return {
     exceptionLogData: async ({ params }) => {
-      const { offset, limit, projectId } = params
-      const { data: { data: { list: alertList, count } } } = await fetchAlertLogs({
-        offset, limit
-      })
-      let productIds = [] // 报警日志里的产品id列表
-      let ruleIds = [] // 报警日志的规则id列表
-      let deviceIds = [] // 触发生成日志的设备id列表
-
-      alertList.forEach(item => {
-        if (item) {
-          const { product_id, rule_id, device_id } = item
-          if (!productIds.includes(product_id)) {
-            productIds.push(product_id)
-          }
-          if (!ruleIds.includes(rule_id)) {
-            ruleIds.push(rule_id)
-          }
-          if (!deviceIds.includes(device_id)) {
-            deviceIds.push(device_id)
-          }
-        }
-      })
-
-      // 产品id与数据端点的映射关系
-      let dataPointMap = {}
-      productIds.forEach(async id => {
-        if (!dataPointMap[id]) {
-          const { data: datapointList } = await fetchDataPoints(id)
-          dataPointMap[id] = datapointList
-        }
-      })
-
-      // 获取规则中的设备引用，用于获取设备单独设置的异常条件
-      const referenceDevices = await fetchRuleDeviceReference(ruleIds, deviceIds)
-      // 获取日志中的告警规则列表
-      const alertRules = await fetchAlertRules(projectId, ruleIds)
-
-      alertList.forEach(item => {
-        // 获取引用设备，用于获取设备单独设置的异常条件
-        const findReferenceDevice = referenceDevices.find(device => {
-          return device.rule_id === item.rule_id && device.device_id === item.device_id
+      try {
+        const { offset, limit, order, projectId } = params
+        const { data: { data: { list: alertList, count } } } = await fetchAlertLogs(projectId, {
+          offset, limit, order,
+          filter: [
+            "id", 
+            "device_id", 
+            "rule_id", 
+            "status", 
+            "content", 
+            "count", 
+            "start_time", 
+            "new_time", 
+            "mac", 
+            "notification_content", 
+            "content", 
+            "tag_id", 
+            "notify_type", 
+            "alarm_state_id", 
+            "product_id", 
+            "product_name", 
+            "organization_id", 
+            "recover_time", 
+            "processed_time", 
+            "device_name", 
+            "processing", 
+            "processed"
+          ]
         })
-        if (findReferenceDevice) {
-          item['reference_device'] = findReferenceDevice
-        }
-        // 获取告警规则
-        const findRule = alertRules.find(rule => rule.id === item.rule_id)
-        if (findRule) {
-          item['rule'] = findRule
-        }
-        // 获取告警产品数据端点
-        if (dataPointMap[item.product_id]) {
-          item['datapoints'] = dataPointMap[item.product_id]
-        }
-      })
+        let productIds = [] // 报警日志里的产品id列表
+        let ruleIds = [] // 报警日志的规则id列表
+        let deviceIds = [] // 触发生成日志的设备id列表
 
-      return {
-        list: alertList, 
-        count
+        alertList.forEach(item => {
+          if (item) {
+            const { product_id, rule_id, device_id } = item
+            if (!productIds.includes(product_id)) {
+              productIds.push(product_id)
+            }
+            if (!ruleIds.includes(rule_id)) {
+              ruleIds.push(rule_id)
+            }
+            if (!deviceIds.includes(device_id)) {
+              deviceIds.push(device_id)
+            }
+          }
+        })
+
+        // 产品id与数据端点的映射关系
+        let dataPointMap = {}
+        const dataPointPromises = productIds.map(id => new Promise(async (resolve, reject) => {
+          try {
+            dataPointMap[id] = await fetchDataPoints(id)
+            resolve()
+          } catch (e) {
+            reject(e)
+          }
+        }))
+        await Promise.all(dataPointPromises)
+
+        // 获取规则中的设备引用，用于获取设备单独设置的异常条件
+        const referenceDevices = await fetchRuleDeviceReference(ruleIds, deviceIds)
+        // 获取日志中的告警规则列表
+        const alertRules = await fetchAlertRules(projectId, ruleIds)
+
+        alertList.forEach(item => {
+          if (item) {
+            // 获取引用设备，用于获取设备单独设置的异常条件
+            const findReferenceDevice = referenceDevices.find(device => {
+              return device.rule_id === item.rule_id && device.device_id === item.device_id
+            })
+            if (findReferenceDevice) {
+              item['reference_device'] = findReferenceDevice
+            }
+            // 获取告警规则
+            const findRule = alertRules.find(rule => rule.id === item.rule_id)
+            if (findRule) {
+              item['rule'] = findRule
+            }
+            // 获取告警产品数据端点
+            if (dataPointMap[item.product_id]) {
+              item['datapoints'] = dataPointMap[item.product_id]
+            }
+          }
+        })
+
+        return {
+          list: alertList,
+          count
+        }
+      } catch (error) {
+        console.log(error)
+        return error
       }
     }
   }
--- schema.js
+++ schema.js
@@ -1,27 +1,67 @@
 const { buildSchema } = require('graphql')
 
 module.exports = buildSchema(`
+  type DeviceCondition {
+    compare: Int
+    param: String
+    value: String
+  }
+  type DeviceException {
+    conditions: [DeviceCondition]
+  }
   type ReferenceDevice {
     id: ID
     device_id: ID
     device_mac: String
     exception_enabled: Boolean
+    exception: DeviceException
     notification_enabled: Boolean
     product_id: ID
     rule_id: ID
   }
+  type RuleException {
+    tag_id: String
+    suggestions: String
+  }
   type Rule {
     id: ID
+    compare: Int
+    content: String
+    exception: RuleException
+    interval: Int
+    name: String
+    notification_content: String
+    param: String
+    product_id: String
+    type: Int
+    value: String
   }
   type Datapoint {
-
+    id: ID
+    field_name: String
+    en_field_name: String
+    index: Int
+    is_collect: Boolean
+    is_read: Boolean
+    is_system: Boolean
+    is_write: Boolean
+    max: Int
+    min: Int
+    name: String
+    symbol: String
+    type: Int
   }
   type Log {
     id: ID
-    name: String
-    content: String
-    param: String
+    status: Int
     product_id: String
+    device_name: String
+    device_id: Int
+    count: Int
+    content: String
+    new_time: String
+    recover_time: String
+    start_time: String
     reference_device: ReferenceDevice
     rule: Rule
     datapoints: [Datapoint]
@@ -30,9 +70,13 @@ module.exports = buildSchema(`
     list: [Log]
     count: Int
   }
+  input Order {
+    new_time: String
+  }
   input ExceptionLogParams {
     offset: Int
     limit: Int
+    order: Order
     projectId: ID
   }
   type Query {
